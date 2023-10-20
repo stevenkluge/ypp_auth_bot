b0c471601f82f03a7116f43b35acece1b2dc6750
Steven Kluge <kluge826@gmail.com>
2023-10-19 20:59:25 -0400

    In-progress: Initial implementation
    Implemented database usage

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,38 @@
 import 'dotenv/config'
 import { Client, Events, GatewayIntentBits } from 'discord.js'
+import  * as sqlite from 'sqlite3'
+import { logger } from './logger'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+logger.info('Starting YPP Pirate Verifier Bot')
 
-client.once(Events.ClientReady, c => console.log(`Bot ready, logged in as ${c.user.tag}`))
+// initialize database connection and export
+export let database = new sqlite.Database(process.env.DATABASE_FILE, (err) => {
+    if(err === null) {
+        logger.info(`Connection to database "${process.env.DATABASE_FILE}" established`)
 
-client.login(process.env.DISCORD_TOKEN)
+        // register clean-up and logging events on any exit condition
+        const events = ['uncaughtException', 'SIGTERM', 'SIGINT', 'SIGHUP', 'beforeExit'];
+        events.forEach((type => {
+            process.once(type, (type) => {
+                logger.info(`Received signal: "${type}". Beginning clean-up`)
+                database.close((err) => {
+                    if(err === null) {
+                        logger.info(`Database "${process.env.DATABASE_FILE}" closed successfully`)
+                    } else {
+                        logger.error(`Encountered an error trying to close database connection: ${err}`)
+                    }
+                    process.exit(0);
+                })
+                
+            })
+        }))
+
+        // login to discord bot
+        const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+        client.once(Events.ClientReady, c => console.log(`Logging into discord as: ${c.user.tag}`))
+        client.login(process.env.DISCORD_TOKEN)
+        
+    } else {
+        logger.error(`Encountered an error trying to open database connection from file "${process.env.DATABASE_FILE}": ${err}`)
+    }
+})
--- src/yowebScraper.ts
+++ src/yowebScraper.ts
@@ -1,13 +1,13 @@
 import * as cheerio from 'cheerio'
 import { logger } from './logger'
 import 'dotenv/config'
+import { database } from './app'
 
 /*
 How the scraper will be used:
 -----------------------------
 /link pirate
-    fetch pirate document
-    parse crew, rank
+    fetch pirate document => getPirateCrewRank() returns crew, rank
 
 /link verify
     fetch trophy document
@@ -22,39 +22,28 @@ swampyy
 
 */
 
-export class YowebScraper {
-    private pirate: string;
-    private crew: string;
-    private rank: string;
-    private trophyCollectionBoxNames: string[];
+export const getPirateCrewRank = async (pirate: string): Promise<{ crew: string, rank: string }> {
 
-    constructor(pirate: string) {
-        this.pirate = pirate;
+    try {
+        const document = await fetchDocument(`http://emerald.puzzlepirates.com/yoweb/pirate.wm?target=${pirate}`)
+        const $ = cheerio.load(document)
+        const crewInfo = $('body > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > font:nth-child(1)')
+        // TODO get the first <b> tag for rank, second <b> tag get the child <a> tag href value then regex match the id from address
+        const 
+        //example address http://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=5036718&classic=$classic <- match 5036718
+
+    } catch(err) {
+        logger.error(err)
     }
 
-    async getPirateCrewInfo(): Promise<{ crew: string, rank: string }> {
-
-        try {
-            const document = await this.fetchDocument(`http://emerald.puzzlepirates.com/yoweb/pirate.wm?target=${this.pirate}`)
-            const $ = cheerio.load(document)
-            const crewInfo = $('body > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > font:nth-child(1)')
-            // TODO get the first <b> tag for rank, second <b> tag get the child <a> tag href value then regex match the id from address
-            //example address http://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=5036718&classic=$classic <- match 5036718
-
-        } catch(err) {
-            logger.error(err)
-        }
 
 
+}
 
-    }
+const fetchDocument = async (resourceUrl: string) => {
+    const response = await fetch(resourceUrl)
 
-    private fetchDocument = async (resourceUrl: string) => {
-        const response = await fetch(resourceUrl)
+    if(!response.ok) throw new Error(`'fetch(${resourceUrl}') returned ${response.status}: ${response.statusText}`)
 
-        if(!response.ok) throw new Error(`'fetch(${resourceUrl}') returned ${response.status}: ${response.statusText}`)
-
-        return response.text()
-    }
-
+    return response.text()
 }
